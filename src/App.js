e9577ec140658ae7335bce6d557cd4c4ef669420
saakshidas <75031142+saakshidas@users.noreply.github.com>
2021-10-01 12:08:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Breakout from "./games/breakout";
+function App() {
+  return (
+   <Router>
+     <Switch>
+       <Route path="/">
+         <Breakout/>
+       </Route>
+     </Switch>
+   </Router>
+  );
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    ballObj: {
+      x: 20,
+      y: 200,
+      dx: 5,
+      dy: 5,
+      rad: 25,
+      speed: 10,
+    },
+    brickObj: {
+      x: 0.5,
+      y: 50,
+      height: 20,
+      density: 9,
+      colors: ["red", "#29465B"],
+    },
+    player: {
+      name: "sakshi",
+      lives: 5,
+      score: 0,
+      level: 1,
+    },
+    paddleProps: {
+      height: 20,
+      width: 180,
+      x: 100,
+      color: "",
+    },
+  };
+  
